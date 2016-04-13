d9579f2386c82d996b5df45fe1e379eda6c80b69
Nadi Knysh <nadvikn@gmail.com>
2016-04-12 19:06:36 -0700

    Nadiya

---
+++ TestRunner_E2E.bat
@@ -0,0 +1,43 @@
+@ECHO OFF
+::========================================
+SET GITHUB_ACCOUNT=Natestkn
+SET WS_DIR=Workspace
+SET REPO_NAME=Title_Validation_E2E
+SET APP_VERSION=1.1
+SET MAIN_CLASS=core.Selenium
+SET ARGS_01=
+::========================================
+IF "%JAVA_HOME%" == "" GOTO EXIT_JAVA
+ECHO Java installed
+IF "%M2%" == "" GOTO EXIT_MVN
+ECHO Maven installed
+CALL git --version > nul 2>&1
+IF NOT %ERRORLEVEL% == 0 GOTO EXIT_GIT
+ECHO Git installed
+GOTO NEXT
+:NEXT
+IF NOT EXIST C:\%WS_DIR% GOTO NO_WORKSPACE
+IF EXIST C:\%WS_DIR%\%REPO_NAME% RMDIR /S /Q C:\%WS_DIR%\%REPO_NAME%
+CD C:\%WS_DIR%
+git clone https://github.com/%GITHUB_ACCOUNT%/%REPO_NAME%.git
+CD %REPO_NAME%
+SLEEP 2
+CALL mvn clean install site test -Dtest=AllTests -Dbuild.version="1.1"
+ECHO.
+ECHO Executing Java programm ...
+java -jar C:\%WS_DIR%\%REPO_NAME%\target\%REPO_NAME%-%APP_VERSION%.jar %ARG_01%
+GOTO END
+:EXIT_JAVA
+ECHO No Java installed
+GOTO END
+:EXIT_MVN
+ECHO No Maven installed
+END
+:EXIT_GIT
+ECHO No Git installed
+GOTO END
+:NO_WORKSPACE
+ECHO %WS_DIR% is not exists
+GOTO END
+:END
+CD\
